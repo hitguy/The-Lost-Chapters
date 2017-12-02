1aadae5dc07d5313fe8937c291d3e71e0d0d5492
Thanos Zouganelis <Thanos Zouganelis>
2017-12-02 17:23:18 +0000

    Added Travis CI
    Travis
    
    Travis again
    
    Travis again..
    
    Travis bla
    
    Trravis
    
    Travis again...
    
    Travis
    
    Travis
    
    Built
    
    Bla
    
    B
    
    Bla

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,11 +1,21 @@
 #! /bin/sh
 
-# Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+# Download Unity3D installer into the container
+#  The below link will need to change depending on the version, this one is for 5.5.1
+#  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
+echo 'Downloading Unity 2017.2.0 pkg:'
+curl --retry 5 -o Unity.pkg https://netstorage.unity3d.com/unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg
+if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
-# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
-# installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+# In Unity 5 they split up build platform support into modules which are installed separately
+# By default, only Mac OSX support is included in the original editor package; Windows, Linux, iOS, Android, and others are separate
+# In this example we download Windows support. Refer to http://unity.grimdork.net/ to see what form the URLs should take
+echo 'Downloading Unity 2017.2.0 Windows Build Support pkg:'
+curl --retry 5 -o Unity_win.pkg https://beta.unity3d.com/download/46dda1414e51/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.2.0f3.pkg
+if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
+# Run installer(s)
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
+echo 'Installing Unity_win.pkg'
+sudo installer -dumplog -package Unity_win.pkg -target /
